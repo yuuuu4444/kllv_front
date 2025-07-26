4ee3354147c04a8967af71f63833c7832636fe70
goobo251111111 <goobo251111111@gmail.com>
2025-07-26 19:54:10 +0800

    feat(header, footer): 新增電腦版 header & footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,106 @@
   import { RouterLink } from 'vue-router';
 </script>
 
-<template></template>
+<template>
+  <footer>
+    <div class="main">
+      <div class="main__logo">
+        <RouterLink to="/index">
+          <img src="/src/assets/image/logo.png" alt="logo">
+        </RouterLink>
+      </div>
+      <ul class="main__list" role="navigation">
+        <li class="main__item">
+          <RouterLink to="/news">
+            <h5 class="medium">最新消息</h5>
+          </RouterLink>
+        </li>
+        <li class="main__item">
+          <RouterLink to="/about">
+            <h5 class="medium">關於社區</h5>
+          </RouterLink>
+        </li>
+        <li class="main__item">
+          <RouterLink to="/events">
+            <h5 class="medium">活動報名</h5>
+          </RouterLink>
+        </li>
+        <li class="main__item">
+          <RouterLink to="/repair">
+            <h5 class="medium">維修通報</h5>
+          </RouterLink>
+        </li>
+      </ul>
+      <div class="main__info">
+        <h5 class="medium">聯絡資訊</h5>
+        <h5 class="regular">聯絡電話：0987654321</h5>
+        <h5 class="regular">聯絡信箱：konlongzhood@gmail.com</h5>
+        <h5 class="regular">聯絡地址：桃園市中壢區空瀧浪里孔融路1號</h5>
+      </div>
+    </div>
+    <div class="disclaimer">
+      <p class="body--c1">
+        本網站為緯育 TibaMe 前端工程師專業技術養成班第 92 期學員專題成果作品，本平台僅供學習、展示之用。
+        若有抵觸有關著作權，或有第三人主張侵害智慧財產權等情事，均由學員負法律上責任，緯育公司概不負責。
+        若有侵權疑慮，請私訊緯育 TibaMe，後續會由專人協助處理。
+      </p>
+    </div>
+  </footer>
+</template>
 
-<style scoped></style>
+<style scoped lang="scss">
+  @import "@/assets/scss/style";
+
+  footer {
+    // position: fixed;
+    // bottom: 0;
+    // left: 0;
+    width: 100%;
+    height: 28vw;
+    z-index: 99;
+
+    background-color: $primary-c000;
+    background-image: url(/src/assets/image/footer.png);
+
+    .main {
+      width: 100%;
+      height: 23vw;
+      padding: 0 6.252vw;
+      @include flex-space-between;
+
+      &__logo {
+        width: 21vw;
+
+        img {
+          width: 100%;
+          height: auto;
+        }
+      }
+
+      &__list, &__info {
+        display: flex;
+        flex-direction: column;
+        gap: 1.25vw;
+        
+        h5{
+          font-size: 1.25vw;
+        }
+        h5.medium {
+          color: black;
+          font-weight: 500;
+          letter-spacing: 0.1em;
+        }
+      }
+    }
+
+    .disclaimer {
+      height: 5vw;
+      border-top: 1px solid black;
+      @include flex-center;
+
+      p {
+        font-size: 0.625vw;
+      }
+    }
+  }
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,186 @@
 <script setup>
   import { RouterLink } from 'vue-router';
+  import { ref, onMounted, onBeforeUnmount } from 'vue'
+
+  // 開啟子選單
+  const isOpen = ref(false);
+  const openDropdown = () => {
+    isOpen.value = !isOpen.value;
+  };
+
+  // 隱藏導覽列
+  const isHidden = ref(false)
+  const hideHaeder = () => {
+    const scrollY = window.scrollY;
+    const scrollLimit = window.innerHeight / 4;
+    isHidden.value = (scrollY > scrollLimit);
+  };
+  onMounted(() => {
+    window.addEventListener('scroll', hideHaeder)
+  })
+  onBeforeUnmount(() => {
+    window.removeEventListener('scroll', hideHaeder)
+  })
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">首頁</RouterLink>
-        <RouterLink to="/news">最新消息</RouterLink>
-        <RouterLink to="/about">關於社區</RouterLink>
-        <RouterLink to="/">里民服務</RouterLink>
-        <RouterLink to="/">里民開講</RouterLink>
-        <RouterLink to="/member">帳戶管理</RouterLink>
-      </nav>
+  <header :class="{ hidden: isHidden }">
+    <div class="mainbar">
+      <div class="mainbar__logo">
+        <RouterLink to="/index">
+          <img src="/src/assets/image/logo.png" alt="logo">
+        </RouterLink>
+      </div>
+        <ul class="mainbar__list" role="navigation">
+          <li class="mainbar__item">
+            <RouterLink to="/news">
+              <img src="/src/assets/icon/icon_news.png" alt="news-icon">
+              <h5 class="medium">最新消息</h5>
+            </RouterLink>
+          </li>
+          <li class="mainbar__item">
+            <RouterLink to="/about">
+              <img src="/src/assets/icon/icon_about.png" alt="about-icon">
+              <h5 class="medium">關於社區</h5>
+            </RouterLink>
+          </li>
+          <li class="mainbar__item dropdown" @click="openDropdown">
+            <RouterLink>
+              <img src="/src/assets/icon/icon_services.png" alt="services-icon">
+              <h5 class="medium">里民服務</h5>
+            </RouterLink>
+            <ul class="dropdown__list" :style="{ display: isOpen ? 'flex' : 'none' }">
+              <li class="dropdown__item">
+                <RouterLink to="/events">
+                  <h5 class="medium">活動報名</h5>
+                </RouterLink>
+              </li>
+              <li class="dropdown__item">
+                <RouterLink to="/repair">
+                  <h5 class="medium">維修通報</h5>
+                </RouterLink>
+              </li>
+            </ul>
+          </li>
+          <li class="mainbar__item">
+            <RouterLink to="/community">
+              <img src="/src/assets/icon/icon_community.png" alt="community-icon">
+              <h5 class="medium">里民開講</h5>
+            </RouterLink>
+          </li>
+          <li class="mainbar__item">
+            <RouterLink to="/member">
+              <img src="/src/assets/icon/icon_member.png" alt="members-icon">
+              <h5 class="medium">帳戶管理</h5>
+            </RouterLink>
+          </li>
+        </ul>
+    </div>
+    <div class="infobar">
+        <a class="infobar__mobile" href="tel:+886987654321">
+          <img src="/src/assets/icon/icon_mobile.svg" alt="mobile-icon">
+          <h5 class="medium">電話：0987654321</h5>
+        </a>
+        <a class="infobar__mail" href="mailto:konlongzhood@gmail.com?subject=空瀧浪里">
+          <img src="/src/assets/icon/icon_mail.svg" alt="mail-icon">
+          <h5 class="medium">電子信箱</h5>
+        </a>
     </div>
   </header>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  @import "@/assets/scss/style";
+
+  header {
+    position: fixed;
+    top: 30px;
+    left: 3.125vw;
+    width: calc(100% - 6.25vw);
+    z-index: 99;
+    transition: transform 0.5s ease;
+
+    .mainbar {
+      padding: 0.4168vw 2.084vw;
+      @include flex-space-between;
+
+      border-radius: $border-r-md;
+      border-bottom-left-radius: 0;
+      background-color: $white;
+      // background-color: $primary-c000;
+
+      &__logo {
+        width: 21vw;
+        
+        img {
+          width: 100%;
+          height: auto;
+        }
+      }
+
+      &__list {
+        display: flex;
+        gap: 4.168vw;
+
+        img {
+          width: 3.125vw;
+          height: auto;
+          margin: auto;
+        }
+
+        h5.medium {
+          color: black;
+          font-size: 1.25vw;
+          font-weight: 500;
+          letter-spacing: 0.1em;
+        }
+      }
+      .dropdown {
+        position: relative;
+
+        &__list {
+          display: none;
+          flex-direction: column;
+          position: absolute;
+          top: 132%;
+          left: -0.521vw;
+          width: calc(100% + 1.042vw);
+          gap: 1.042vw;
+          padding: 1.042vw 0.521vw;
+          border-radius: 0 0 15px 15px;
+          background-color: $white;
+          // background-color: $primary-c000;
+        
+      }
+      }
+    }
+    .infobar {
+      display: flex;
+      width: 30vw;
+      gap: 2.084vw;
+      padding: 0.521vw 2.084vw;
+      border-radius: 0 0 15px 15px;
+      background-color: $white;
+      // background-color: $primary-c000;
+
+      &__mobile, &__mail {
+        display: flex;
+        align-items: center;
+
+        img {
+          height: 1.5vw;
+        }
+
+        h5.medium {
+          color: $black;
+          font-size: 1.25vw;
+          font-weight: 500;
+          letter-spacing: 0.1em;
+        }
+      }
+    }
+  }
+  header.hidden {
+    transform: translateY(-100%);
+  }
+</style>
