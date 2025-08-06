9b941e681cd1fe62ed8caa98986606339edc69c0
yuuuu4444 <yuuuu850430@gmail.com>
2025-08-06 15:58:23 +0800

    refactor(MemberPosts.vue):修改視覺排版、增加分頁功能

--- src/views/Member/MemberPosts.vue
+++ src/views/Member/MemberPosts.vue
@@ -36,7 +36,7 @@
           </thead>
           <tbody>
             <tr
-              v-for="post in myPosts"
+              v-for="post in paginatedPosts"
               :key="post.post_no"
               class="postTable__row"
               :class="{ 'is-reported': post.is_reported }"
@@ -57,7 +57,7 @@
               <!-- 編輯按鈕 -->
               <td class="postTable__cell--action">
                 <router-link
-                  :to="`/community/post/${post.post_no}/edit`"
+                  :to="`/community/${post.post_no}/edit`"
                   :class="['iconButton', { 'iconButton--disabled': post.is_reported }]"
                   :event="post.is_reported ? '' : 'click'"
                   aria-label="編輯貼文"
@@ -95,7 +95,24 @@
             </tr>
           </tbody>
         </table>
-        <!-- 分頁元件 -->
+        <!-- 分頁 -->
+        <div class="pagination">
+          <button
+            class="pagination__button btn--changepage"
+            @click="postsGoPrev"
+            :disabled="isPostsFirstPage"
+          >
+            <
+          </button>
+          <p class="pagination__current">{{ postsCurrentPage }} / {{ postsTotalPages }}</p>
+          <button
+            class="pagination__button btn--changepage"
+            @click="postsGoNext"
+            :disabled="isPostsLastPage"
+          >
+            >
+          </button>
+        </div>
       </div>
 
       <!-- 2. 已回覆貼文表格 -->
@@ -110,7 +127,7 @@
           </thead>
           <tbody>
             <tr
-              v-for="reply in myReplies"
+              v-for="reply in paginatedReplies"
               :key="reply.comment_no"
               class="postTable__row"
             >
@@ -127,7 +144,24 @@
             </tr>
           </tbody>
         </table>
-        <!-- 分頁元件 -->
+        <!-- 分頁 -->
+        <div class="pagination">
+          <button
+            class="pagination__button btn--changepage"
+            @click="repliesGoPrev"
+            :disabled="isRepliesFirstPage"
+          >
+            <
+          </button>
+          <p class="pagination__current">{{ repliesCurrentPage }} / {{ repliesTotalPages }}</p>
+          <button
+            class="pagination__button btn--changepage"
+            @click="repliesGoNext"
+            :disabled="isRepliesLastPage"
+          >
+            >
+          </button>
+        </div>
       </div>
     </div>
 
@@ -158,12 +192,12 @@
       >
         <h5 class="mobileList__title bold">貼文</h5>
         <div
-          v-for="post in myPosts"
+          v-for="post in paginatedPosts"
           :key="post.post_no"
           class="mobileList__item"
         >
           <router-link
-            :to="`/community/post/${post.post_no}`"
+            :to="`/community/${post.post_no}`"
             class="mobileList__info"
           >
             <span class="mobileList__date">{{ post.created_at }}</span>
@@ -183,7 +217,24 @@
             </svg>
           </button>
         </div>
-        <!-- 分頁元件 -->
+        <!-- 分頁 -->
+        <div class="pagination">
+          <button
+            class="pagination__button btn--changepage"
+            @click="postsGoPrev"
+            :disabled="isPostsFirstPage"
+          >
+            <
+          </button>
+          <p class="pagination__current">{{ postsCurrentPage }} / {{ postsTotalPages }}</p>
+          <button
+            class="pagination__button btn--changepage"
+            @click="postsGoNext"
+            :disabled="isPostsLastPage"
+          >
+            >
+          </button>
+        </div>
       </div>
       <!-- 第二層：已回覆貼文列表 -->
       <div
@@ -192,13 +243,13 @@
       >
         <h5 class="mobileList__title">已回覆貼文</h5>
         <div
-          v-for="reply in myReplies"
+          v-for="reply in paginatedReplies"
           :key="reply.comment_no"
           class="mobileList__item"
         >
           <router-link
             v-if="!reply.is_deleted"
-            :to="`/forum/post/${reply.post_no}`"
+            :to="`/community/${reply.post_no}`"
             class="mobileList__info"
           >
             <span class="mobileList__date">{{ reply.commented_at }}</span>
@@ -212,7 +263,24 @@
             <span class="mobileList__name is-deleted">{{ reply.original_post_title }}</span>
           </div>
         </div>
-        <!-- 分頁元件 -->
+        <!-- 分頁 -->
+        <div class="pagination">
+          <button
+            class="pagination__button btn--changepage"
+            @click="repliesGoPrev"
+            :disabled="isRepliesFirstPage"
+          >
+            <
+          </button>
+          <p class="pagination__current">{{ repliesCurrentPage }} / {{ repliesTotalPages }}</p>
+          <button
+            class="pagination__button btn--changepage"
+            @click="repliesGoNext"
+            :disabled="isRepliesLastPage"
+          >
+            >
+          </button>
+        </div>
       </div>
     </div>
 
@@ -228,54 +296,62 @@
           class="mobileActions"
         >
           <!-- 只有未被檢舉的貼文才能編輯 -->
-          <router-link
-            v-if="!showActionsModalForPost.is_reported"
-            :to="`/community/post/${showActionsModalForPost.post_no}/edit`"
-            class="mobileActions__item"
-          >
-            <svg
-              class="editIcon"
-              viewBox="0 0 24 24"
+          <template v-if="!showActionsModalForPost.is_reported">
+            <router-link
+              :to="`/community/${showActionsModalForPost.post_no}/edit`"
+              class="mobileActions__item"
             >
-              <path
-                d="M3 17.46V20.5C3 20.78 3.22 21 3.5 21H6.54C6.67 21 6.8 20.95 6.89 20.85L17.81 9.94L14.06 6.19L3.15 17.1C3.05 17.2 3 17.32 3 17.46ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04Z"
-              />
-            </svg>
-            編輯貼文
-          </router-link>
+              <svg
+                class="editIcon"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M3 17.46V20.5C3 20.78 3.22 21 3.5 21H6.54C6.67 21 6.8 20.95 6.89 20.85L17.81 9.94L14.06 6.19L3.15 17.1C3.05 17.2 3 17.32 3 17.46ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04Z"
+                />
+              </svg>
+              編輯貼文
+            </router-link>
+            <div
+              @click="deletePost(showActionsModalForPost.post_no)"
+              class="mobileActions__item is-danger"
+            >
+              <svg
+                class="deleteIcon"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M18 5V19C18 19.5 17.5 20 17 20H12H7C6.5 20 6 19.5 6 19V5"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M4 5H20"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M10 4H14M10 9V16M14 9V16"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              刪除貼文
+            </div>
+          </template>
+          <!-- 被檢舉顯示題是訊息 -->
           <div
-            @click="deletePost(showActionsModalForPost.post_no)"
-            class="mobileActions__item is-danger"
+            v-else
+            class="mobileActions__item is-disabled"
           >
-            <svg
-              class="deleteIcon"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M18 5V19C18 19.5 17.5 20 17 20H12H7C6.5 20 6 19.5 6 19V5"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M4 5H20"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M10 4H14M10 9V16M14 9V16"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            刪除貼文
+            此貼文已被檢舉，無法進行操作
           </div>
         </div>
       </template>
@@ -284,7 +360,7 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
   import { useRouter } from 'vue-router';
   import MemberModal from '@/components/MemberModal.vue';
   import MemberMobileHeader from '@/components/MemberMobileHeader.vue';
@@ -300,14 +376,14 @@
     {
       post_no: 1, //  (PK)
       author_id: 'user_account_001',
-      title: '好多市冷凍藍莓有人想一起湊嗎？',
+      title: '多出來的書要交換嗎？',
       created_at: '2025-07-14', // 此欄位需要json或後端處理
       is_reported: false,
     },
     {
       post_no: 2,
       author_id: 'user_account_001',
-      title: '半夜肚子好餓哦大家宵夜都吃啥',
+      title: '公共廁所太髒了',
       created_at: '2025-06-20',
       is_reported: false,
     },
@@ -318,11 +394,25 @@
       created_at: '2025-06-19',
       is_reported: true, // 假設被檢舉下架
     },
+    {
+      post_no: 4,
+      author_id: 'user_account_001',
+      title: '多出來的書要交換嗎？',
+      created_at: '2025-06-18',
+      is_reported: false,
+    },
+    {
+      post_no: 5,
+      author_id: 'user_account_001',
+      title: '想買飲水機來團嗎？',
+      created_at: '2025-06-25',
+      is_reported: true,
+    },
   ]);
 
   const myReplies = ref([
     {
-      comment_no: 201, // 留言編號 (PK)
+      comment_no: 1, // 留言編號 (PK)
       post_no: 1, // 所屬貼文編號 (FK)
       commenter_id: 'user_account_001',
       commented_at: '2025-07-14',
@@ -330,7 +420,7 @@
       is_original_post_deleted: false, // 原文是否已删除 (此欄位還需要json或後端判斷)
     },
     {
-      comment_no: 202,
+      comment_no: 2,
       post_no: 2,
       commenter_id: 'user_account_001',
       commented_at: '2025-06-20',
@@ -338,14 +428,73 @@
       is_original_post_deleted: false,
     },
     {
-      comment_no: 203,
+      comment_no: 3,
       post_no: 999,
       commenter_id: 'user_account_001',
       commented_at: '2025-06-19',
       original_post_title: '好想睡哦哦',
       is_original_post_deleted: true, // 假設此貼文已刪除
     },
+    {
+      comment_no: 4,
+      post_no: 2,
+      commenter_id: 'user_account_001',
+      commented_at: '2025-06-24',
+      original_post_title: '太髒了',
+      is_original_post_deleted: false,
+    },
+    {
+      comment_no: 2,
+      post_no: 2,
+      commenter_id: 'user_account_001',
+      commented_at: '2025-06-21',
+      original_post_title: '怎麼路燈又壞了',
+      is_original_post_deleted: false,
+    },
   ]);
+
+  // --- 分頁 ---
+  // 1. 我的貼文
+  const postsPageSize = ref(3); // 每頁顯示幾筆
+  const postsCurrentPage = ref(1); // 當前頁碼
+  // 計算總頁數
+  const postsTotalPages = computed(() => Math.ceil(myPosts.value.length / postsPageSize.value));
+  // 計算當前是否為第一頁/最後一頁
+  const isPostsFirstPage = computed(() => postsCurrentPage.value === 1);
+  const isPostsLastPage = computed(() => postsCurrentPage.value === postsTotalPages.value);
+  // 計算屬性：從完整列表中“切割”出當前頁要顯示的資料
+  const paginatedPosts = computed(() => {
+    const startIndex = (postsCurrentPage.value - 1) * postsPageSize.value;
+    const endIndex = startIndex + postsPageSize.value;
+    return myPosts.value.slice(startIndex, endIndex);
+  });
+  // 換頁函式
+  const postsGoPrev = () => {
+    if (!isPostsFirstPage.value) postsCurrentPage.value--;
+  };
+  const postsGoNext = () => {
+    if (!isPostsLastPage.value) postsCurrentPage.value++;
+  };
+
+  // 2. 已回覆貼文
+  const repliesPageSize = ref(3);
+  const repliesCurrentPage = ref(1);
+  const repliesTotalPages = computed(() =>
+    Math.ceil(myReplies.value.length / repliesPageSize.value),
+  );
+  const isRepliesFirstPage = computed(() => repliesCurrentPage.value === 1);
+  const isRepliesLastPage = computed(() => repliesCurrentPage.value === repliesTotalPages.value);
+  const paginatedReplies = computed(() => {
+    const startIndex = (repliesCurrentPage.value - 1) * repliesPageSize.value;
+    const endIndex = startIndex + repliesPageSize.value;
+    return myReplies.value.slice(startIndex, endIndex);
+  });
+  const repliesGoPrev = () => {
+    if (!isRepliesFirstPage.value) repliesCurrentPage.value--;
+  };
+  const repliesGoNext = () => {
+    if (!isRepliesLastPage.value) repliesCurrentPage.value++;
+  };
 
   // --- 事件處理函式 ---
   const goBackToMenu = () => router.push('/member');
@@ -377,7 +526,11 @@
     }
   }
 
-  /* --- 桌面版樣式 --- */
+  // 預設隱藏手機
+  .postsPage__mobileView {
+    display: none;
+  }
+
   .postSection {
     margin-bottom: 40px;
     &__title {
@@ -429,11 +582,10 @@
         border-bottom: none;
       }
 
-      // 被檢舉行的樣式
+      // 被檢舉的樣式
       &.is-reported {
         color: $neutral-c;
         cursor: not-allowed;
-        pointer-events: none;
         &:hover {
           background-color: transparent;
         }
@@ -460,7 +612,7 @@
     }
   }
 
-  /* --- icon按鈕 --- */
+  //按鈕
   .iconButton {
     background: none;
     border: none;
@@ -490,102 +642,40 @@
     }
   }
 
-  /* --- 手機版樣式 --- */
-  .mobileIndex {
-    &__item {
-      font-size: 18px;
-      padding: 20px 16px;
-      border-bottom: 1px solid $primary-c100;
-      cursor: pointer;
-      // &:hover {
-      //   background-color: $primary-c50;
-      // }
-      &:last-child {
-        border-bottom: none;
-      }
-    }
-  }
-  .mobileList {
-    &__title {
-      color: $primary-c700;
+  //分頁
+  .pagination {
+    @include flex-center;
+    gap: 30px;
+    margin: 30px 0;
+
+    &__current {
       text-align: center;
-      margin: 24px;
-    }
-    &__item {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding: 16px;
-      border-bottom: 1px solid $primary-c100;
-      &:last-child {
-        border-bottom: none;
-      }
-    }
-    &__info {
-      display: flex;
-      flex-direction: column;
-      gap: 4px;
-      text-decoration: none;
-      color: inherit;
-    }
-    &__date {
       font-size: 14px;
-      color: $neutral-c;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 22px;
+      letter-spacing: 0.1em;
+      width: 80px;
+      padding: 14px 0;
+      border: 1px solid $black;
+      border-radius: $border-r-xs;
+      background-color: $white;
     }
-    &__name {
-      // font-weight: 500;
-      line-height: 1.4;
-      color: $primary-c500;
-      &.is-deleted {
-        color: $neutral-c;
-        text-decoration: line-through;
-      }
-    }
-    .moreIcon {
-      fill: $neutral-c;
-    }
-  }
 
-  // 手機版操作彈窗
-  .mobileActions {
-    display: flex;
-    flex-direction: column;
-    &__item {
-      display: flex;
-      align-items: center;
-      gap: 12px;
-      padding: 16px;
-      font-size: 16px;
-      cursor: pointer;
-      text-decoration: none;
-      color: $black;
+    &__button:disabled {
+      background-color: $neutral-c;
+      cursor: not-allowed;
       &:hover {
-        background-color: $primary-c50;
-      }
-      &.is-danger {
-        color: $semantic-r;
-      }
-      svg {
-        width: 20px;
-        height: 20px;
-        &.editIcon {
-          fill: $black;
-        }
-        // &.deleteIcon {
-        //   fill: $semantic-r;
-        // }
+        background-color: $neutral-c;
       }
     }
   }
 
-  // 預設隱藏手機
-  .postsPage__mobileView {
-    display: none;
-  }
-
-  // 局部媒體查詢，不受 mixin 影響
-  @media screen and (max-width: 767px) {
+  @include mobile {
     .postsPage {
+      &__title {
+        display: block;
+      }
       &__desktopView {
         display: none;
       }
@@ -593,13 +683,101 @@
       &__mobileView {
         display: block;
       }
-
-      &__title {
-        display: none;
+    }
+    .mobileIndex {
+      &__item {
+        font-size: 18px;
+        padding: 20px 16px;
+        border-bottom: 1px solid $primary-c100;
+        cursor: pointer;
+        // &:hover {
+        //   background-color: $primary-c50;
+        // }
+        &:last-child {
+          border-bottom: none;
+        }
       }
     }
-    .moreIcon {
-      width: 24px;
+    .mobileList {
+      display: block;
+      &__title {
+        color: $primary-c700;
+        text-align: center;
+        margin: 24px;
+      }
+      &__item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 16px;
+        border-bottom: 1px solid $primary-c100;
+        &:last-child {
+          border-bottom: none;
+        }
+      }
+      &__info {
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+        text-decoration: none;
+        color: inherit;
+      }
+      &__date {
+        font-size: 14px;
+        color: $neutral-c;
+      }
+      &__name {
+        // font-weight: 500;
+        line-height: 1.4;
+        color: $primary-c500;
+        &.is-deleted {
+          color: $neutral-c;
+          text-decoration: line-through;
+        }
+      }
+      .moreIcon {
+        width: 24px;
+        fill: $neutral-c;
+      }
+    }
+
+    // 手機版操作彈窗
+    .mobileActions {
+      display: flex;
+      flex-direction: column;
+      &__item {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+        padding: 16px;
+        font-size: 16px;
+        cursor: pointer;
+        text-decoration: none;
+        color: $black;
+        // &:hover {
+        //   background-color: $primary-c50;
+        // }
+        &.is-danger {
+          color: $semantic-r;
+        }
+        &.is-disabled {
+          color: $neutral-c;
+          cursor: not-allowed;
+          &:hover {
+            background-color: transparent;
+          }
+        }
+        svg {
+          width: 24px;
+          height: 24px;
+          &.editIcon {
+            fill: $black;
+          }
+          // &.deleteIcon {
+          //   fill: $semantic-r;
+          // }
+        }
+      }
     }
   }
 </style>
