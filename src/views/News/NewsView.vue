1af2477d0fdc02a4ef9021921b27dbdcb6da889e
goobo251111111 <goobo251111111@gmail.com>
2025-08-06 16:56:22 +0800

    refactor(NewsView, NewsDetails): 修改麵包屑

--- src/views/News/NewsView.vue
+++ src/views/News/NewsView.vue
@@ -54,96 +54,97 @@
     title="最新消息"
     titleAos="fade"
   />
-  <div class="container">
-    <div class="breadcrumb">
-      <RouterLink to="/index">
-        <p class="body--b3">首頁</p>
-      </RouterLink>
-      <p class="body--b3">/</p>
-      <RouterLink to="/news">
-        <p class="body--b3">最新消息</p>
-      </RouterLink>
-    </div>
-    <div class="newsboard">
-      <div
-        class="newsboard__tags"
-        data-aos="fade"
-      >
-        <button
-          v-for="tag in newsTags"
-          :key="tag.id"
-          :class="`newsboard__tag btn--tab${tag.type} { active: selectedTag == tag.name }`"
-          @click="changeTag(tag)"
-        >
-          {{ tag.name }}
-        </button>
-      </div>
-      <div class="newsboard__tags--select">
-        <select v-model="selectedTag">
-          <option
-            v-for="tag in newsTags"
-            :key="tag.id"
-            :value="tag.name"
-          >
-            {{ tag.name }}
-          </option>
-        </select>
-      </div>
-      <div
-        class="newsboard__posts"
-        data-aos="fade"
-      >
-        <RouterLink
-          v-for="post in currentPosts"
-          :key="post.id"
-          :to="`/news/${post.id}`"
-          class="newsboard__post"
-        >
-          <!-- desktop -->
-          <div class="newsboard__tag--d">
-            <p :class="`btn--tab${post.type}`">{{ post.tag }}</p>
-          </div>
-          <div class="newsboard__title--d">
-            <h5 class="regular">{{ post.title }}</h5>
-          </div>
-          <div class="newsboard__date--d">
-            <p class="body--b3">{{ post.date }}</p>
-          </div>
-
-          <!-- mobile -->
-          <div class="newsboard__info--m">
-            <div class="newsboard__tag--m">
-              <p :class="`btn--tab${post.type}`">{{ post.tag }}</p>
-            </div>
-            <div class="newsboard__date--m"><p class="body--b3">｜</p></div>
-            <div class="newsboard__date--m">
-              <p class="body--b3">{{ post.date }}</p>
-            </div>
-          </div>
-          <div class="newsboard__title--m">
-            <h2 class="regular">{{ post.title }}</h2>
-          </div>
+  <div class="newspage">
+    <div class="newspage__container">
+      <div class="breadcrumb">
+        <RouterLink to="/">
+          <p class="breadcrumb__home body--b2">首頁</p>
+        </RouterLink>
+        <RouterLink to="/news">
+          <p class="body--b2">&#47;最新消息</p>
         </RouterLink>
       </div>
-    </div>
-    <div
-      class="pagination"
-      data-aos="fade"
-    >
-      <div
-        class="pagination__prev btn--changepage"
-        @click="prevPage"
-        :disabled="currentPage === 1"
-      >
-        <
-      </div>
-      <div class="pagination__status btn--page">{{ currentPage }} / {{ totalPages }}</div>
-      <div
-        class="pagination__next btn--changepage"
-        @click="nextPage"
-        :disabled="currentPage === totalPages"
-      >
+      <div class="newsboard">
+        <div
+          class="newsboard__tags"
+          data-aos="fade"
         >
+          <button
+            v-for="tag in newsTags"
+            :key="tag.id"
+            :class="`newsboard__tag btn--tab${tag.type} { active: selectedTag == tag.name }`"
+            @click="changeTag(tag)"
+          >
+            {{ tag.name }}
+          </button>
+        </div>
+        <div class="newsboard__tags--select">
+          <select v-model="selectedTag">
+            <option
+              v-for="tag in newsTags"
+              :key="tag.id"
+              :value="tag.name"
+            >
+              {{ tag.name }}
+            </option>
+          </select>
+        </div>
+        <div
+          class="newsboard__posts"
+          data-aos="fade"
+        >
+          <RouterLink
+            v-for="post in currentPosts"
+            :key="post.id"
+            :to="`/news/${post.id}`"
+            class="newsboard__post"
+          >
+            <!-- desktop -->
+            <div class="newsboard__tag--d">
+              <p :class="`btn--tab${post.type}`">{{ post.tag }}</p>
+            </div>
+            <div class="newsboard__title--d">
+              <h5 class="regular">{{ post.title }}</h5>
+            </div>
+            <div class="newsboard__date--d">
+              <p class="body--b3">{{ post.date }}</p>
+            </div>
+
+            <!-- mobile -->
+            <div class="newsboard__info--m">
+              <div class="newsboard__tag--m">
+                <p :class="`btn--tab${post.type}`">{{ post.tag }}</p>
+              </div>
+              <div class="newsboard__date--m"><p class="body--b3">｜</p></div>
+              <div class="newsboard__date--m">
+                <p class="body--b3">{{ post.date }}</p>
+              </div>
+            </div>
+            <div class="newsboard__title--m">
+              <h2 class="regular">{{ post.title }}</h2>
+            </div>
+          </RouterLink>
+        </div>
+      </div>
+      <div
+        class="pagination"
+        data-aos="fade"
+      >
+        <div
+          class="pagination__prev btn--changepage"
+          @click="prevPage"
+          :disabled="currentPage === 1"
+        >
+          <
+        </div>
+        <div class="pagination__status btn--page">{{ currentPage }} / {{ totalPages }}</div>
+        <div
+          class="pagination__next btn--changepage"
+          @click="nextPage"
+          :disabled="currentPage === totalPages"
+        >
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -153,193 +154,214 @@
   @import '@/assets/scss/style';
   @import 'aos/dist/aos.css';
 
-  .container {
-    padding: 1.5625vw 18.75vw 6.25vw;
+  .newspage {
     background-color: $primary_c000;
+    &__container {
+      padding: 1.5625vw 18.75vw 6.25vw;
+      background-color: $primary_c000;
 
-    @include desktop {
-      padding-left: 10%;
-      padding-right: 10%;
-    }
-    @include mobile {
-      padding-left: 2%;
-      padding-right: 2%;
-    }
-  }
-
-  .breadcrumb {
-    display: flex;
-    align-items: center;
-    gap: 0.5em;
-
-    p {
-      color: black;
-    }
-  }
-
-  .newsboard {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    margin: 6.25vw 0 3.125vw;
-    gap: 3.125vw;
-
-    &__tags {
-      @include flex-center;
-      width: 100%;
-      // height: 4.168vw;
-      aspect-ratio: 15 / 1;
-      gap: 1.042vw;
-      background-color: $white;
-      border-radius: $border-r-md;
-      @include mobile {
-        display: none;
+      @include desktop {
+        padding-left: 10%;
+        padding-right: 10%;
       }
-
-      button {
-        font-size: clamp(16px, 1.25vw, 24px);
+      @include mobile {
+        padding-left: 2%;
+        padding-right: 2%;
       }
     }
 
-    &__tags--select {
-      display: none;
-      @include mobile {
-        display: block;
-        width: 100%;
-        select {
-          width: 100%;
-          aspect-ratio: 15 / 1;
-          padding: 0.5em;
-          font-size: clamp(16px, 1.25vw, 24px);
-          letter-spacing: 0.1em;
-          background-color: $white;
-          border-radius: $border-r-md;
-          border: none;
-        }
-        option {
-          padding: 0.5em;
-          font-size: clamp(16px, 1.25vw, 24px);
+    .breadcrumb {
+      // display: flex;
+      // align-items: center;
+      // gap: 0.5em;
+      display: flex;
+      align-items: center;
+      gap: 5px;
+      // p {
+      //   color: black;
+      // }
+      p {
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 32px;
+        letter-spacing: 0.2em;
+        color: black;
+        @include mobile {
+          font-size: 16px;
+          line-height: 24px;
           letter-spacing: 0.1em;
         }
       }
+      &__home {
+        color: $primary-c700 !important;
+      }
     }
 
-    &__posts {
+    .newsboard {
       display: flex;
       flex-direction: column;
       width: 100%;
-    }
-
-    &__post {
-      display: flex;
-      align-items: center;
-      width: 100%;
-      padding: 1.5625vw 3.125vw;
+      margin: 6.25vw 0 3.125vw;
       gap: 3.125vw;
-      background-color: $white;
 
-      &:first-child {
-        border-top-left-radius: $border-r-md;
-        border-top-right-radius: $border-r-md;
-      }
-      &:last-child {
-        border-bottom-left-radius: $border-r-md;
-        border-bottom-right-radius: $border-r-md;
-      }
-      &:nth-child(even) {
-        background-color: rgba(255, 255, 255, 0.75);
-      }
-
-      @include mobile {
-        flex-direction: column;
-        align-items: flex-start;
-        padding: 8px 8px 12px;
-        gap: 32px;
-      }
-    }
-
-    &__tag--d {
-      p {
-        font-size: clamp(16px, 1.25vw, 24px);
-      }
-      @include mobile {
-        display: none;
-      }
-    }
-
-    &__title--d {
-      h5 {
-        font-size: clamp(20px, 1.25vw, 24px);
-        width: 33vw;
-        color: $black;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-
-        @include desktop {
-          width: 45vw;
+      &__tags {
+        @include flex-center;
+        width: 100%;
+        // height: 4.168vw;
+        aspect-ratio: 15 / 1;
+        gap: 1.042vw;
+        background-color: $white;
+        border-radius: $border-r-md;
+        @include mobile {
+          display: none;
         }
-      }
-      @include mobile {
-        display: none;
-      }
-    }
 
-    &__date--d {
-      margin-left: auto;
-      p {
-        font-size: clamp(12px, 0.833vw, 16px);
-        color: $black;
-      }
-      @include mobile {
-        display: none;
-      }
-    }
-
-    &__info--m {
-      display: none;
-      @include mobile {
-        display: flex;
-        align-items: center;
-        padding-left: 2px;
-        gap: 4px;
-      }
-    }
-
-    &__tag--m {
-      display: none;
-      @include mobile {
-        display: block;
-        p {
+        button {
           font-size: clamp(16px, 1.25vw, 24px);
         }
       }
-    }
 
-    &__title--m {
-      display: none;
-      @include mobile {
-        display: block;
-        h2 {
-          color: $black;
+      &__tags--select {
+        display: none;
+        @include mobile {
+          display: block;
+          width: 100%;
+          select {
+            width: 100%;
+            aspect-ratio: 15 / 1;
+            padding: 0.5em;
+            font-size: clamp(16px, 1.25vw, 24px);
+            letter-spacing: 0.1em;
+            background-color: $white;
+            border-radius: $border-r-md;
+            border: none;
+          }
+          option {
+            padding: 0.5em;
+            font-size: clamp(16px, 1.25vw, 24px);
+            letter-spacing: 0.1em;
+          }
         }
       }
-    }
 
-    &__date--m {
-      display: none;
-      @include mobile {
-        display: block;
+      &__posts {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+      }
+
+      &__post {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 1.5625vw 3.125vw;
+        gap: 3.125vw;
+        background-color: $white;
+
+        &:first-child {
+          border-top-left-radius: $border-r-md;
+          border-top-right-radius: $border-r-md;
+        }
+        &:last-child {
+          border-bottom-left-radius: $border-r-md;
+          border-bottom-right-radius: $border-r-md;
+        }
+        &:nth-child(even) {
+          background-color: rgba(255, 255, 255, 0.75);
+        }
+
+        @include mobile {
+          flex-direction: column;
+          align-items: flex-start;
+          padding: 8px 8px 12px;
+          gap: 32px;
+        }
+      }
+
+      &__tag--d {
+        p {
+          font-size: clamp(16px, 1.25vw, 24px);
+        }
+        @include mobile {
+          display: none;
+        }
+      }
+
+      &__title--d {
+        h5 {
+          font-size: clamp(20px, 1.25vw, 24px);
+          width: 33vw;
+          color: $black;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+
+          @include desktop {
+            width: 45vw;
+          }
+        }
+        @include mobile {
+          display: none;
+        }
+      }
+
+      &__date--d {
+        margin-left: auto;
         p {
           font-size: clamp(12px, 0.833vw, 16px);
           color: $black;
         }
+        @include mobile {
+          display: none;
+        }
+      }
+
+      &__info--m {
+        display: none;
+        @include mobile {
+          display: flex;
+          align-items: center;
+          padding-left: 2px;
+          gap: 4px;
+        }
+      }
+
+      &__tag--m {
+        display: none;
+        @include mobile {
+          display: block;
+          p {
+            font-size: clamp(16px, 1.25vw, 24px);
+          }
+        }
+      }
+
+      &__title--m {
+        display: none;
+        @include mobile {
+          display: block;
+          h2 {
+            color: $black;
+          }
+        }
+      }
+
+      &__date--m {
+        display: none;
+        @include mobile {
+          display: block;
+          p {
+            font-size: clamp(12px, 0.833vw, 16px);
+            color: $black;
+          }
+        }
       }
     }
-  }
 
-  .pagination {
-    @include flex-center;
-    gap: 1.666vw;
+    .pagination {
+      @include flex-center;
+      gap: 1.666vw;
+    }
   }
 </style>
