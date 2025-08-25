ae918ddcf74d461a2e0d7d85171762383fdf4131
CHIAO919 <crazy520ky0919@gmail.com>
2025-08-26 01:21:25 +0800

    refactor(css):修改頭貼css

--- src/views/Community/CommunityView.vue
+++ src/views/Community/CommunityView.vue
@@ -507,6 +507,10 @@
         width: 25px;
         aspect-ratio: 1;
         border-radius: 50%;
+        overflow: hidden;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         img {
           width: 100%;
         }
